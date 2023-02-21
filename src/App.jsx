65569cfbe2932114ae976ed4dcad404612bae541
NetMtx <e.netmx@outlook.fr>
2023-02-21 20:18:34 +0100

    Debut de Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from "./components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div></div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
